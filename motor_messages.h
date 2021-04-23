7d3c6ca7d7747b84a4d8b1e7c6ede2c33e74545f
Lee Magnusson <lee@unhuman.io>
2021-04-23 08:14:52 -0700

    tuning mode union

--- motor_messages.h
+++ motor_messages.h
@@ -15,6 +15,7 @@ typedef struct {
 
 typedef enum {OPEN, DAMPED, CURRENT, POSITION, TORQUE, IMPEDANCE, VELOCITY, 
     CURRENT_TUNING, POSITION_TUNING, VOLTAGE, PHASE_LOCK, STEPPER_TUNING, 
+    STEPPER_VELOCITY, 
     CRASH=254, BOARD_RESET=255} MotorMode;
 
 typedef enum {SINE, SQUARE, TRIANGLE, CHIRP} TuningMode;
@@ -22,9 +23,46 @@ typedef enum {SINE, SQUARE, TRIANGLE, CHIRP} TuningMode;
 typedef struct {
     uint32_t host_timestamp;            // Value from host
     uint8_t mode_desired;               // \sa ModeDesired
-    float current_desired;              // motor current desired in A line-line
-    float position_desired;             // motor position desired in rad
-    float velocity_desired;             // motor velocity desired in rad/s
-    float torque_desired;               // torque desired Nm
-    float reserved;                     // reserved for strange uses
+    union {
+        // main control modes
+        struct {                                
+            float current_desired;              // motor current desired in A line-line
+            float position_desired;             // motor position desired in rad
+            float velocity_desired;             // motor velocity desired in rad/s
+            float torque_desired;               // torque desired Nm
+            float reserved;                     // reserved for strange uses
+        };
+        
+        // debug/tuning modes
+        struct {
+            float voltage_desired;
+            float reserved[4];
+        } voltage;
+        struct {
+            uint32_t mode;
+            float amplitude;
+            float reserved;
+            float bias;
+            float frequency;
+        } position_tuning;
+        struct {
+            float amplitude;
+            uint32_t mode;
+            float reserved;
+            float bias;
+            float frequency;
+        } current_tuning;
+        struct {
+            uint32_t mode;
+            float amplitude;
+            float kv;
+            float bias;
+            float frequency;
+        } stepper_tuning;
+        struct {
+            float voltage;
+            float velocity;
+            float reserved[3];
+        } stepper_velocity;
+    };
 } MotorCommand;
