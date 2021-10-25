8e8fb1ed70c77b44e588bc86386885c62b7acdeb
Lee Magnusson <lee@unhuman.io>
2021-10-25 12:32:34 -0700

    version 1.1 add motor flags

--- motor_messages.h
+++ motor_messages.h
@@ -1,6 +1,36 @@
 #pragma once
 
-#define MOTOR_MESSAGES_VERSION  "1.0"
+// The MOTOR_MESSAGES minor number will increment for non breaking changes (i.e. 
+// only adding fields or context) and will increment the major number if there is 
+// a struct reorganization
+#define MOTOR_MESSAGES_VERSION  "1.1"
+
+// The structs below are used for direct communication with the STM32 microcontroller
+// The STM32 is 32 bit little-endian so the packing of these structs will follow.
+// 32 bit alignment by default will cause 32 bit fields to be aligned leaving 
+// empty space after 8 or 16 bit fields. Fortunately this packing seems to work 
+// natively without additional translahtion on current intel, arm, and esp32 processors.
+
+// Each of the below errors can be individually mapped to either force the mode into
+// safe_mode or be ignored and only reported. MotorErrors are latching and can only
+// be cleared by a reset. More detailed error information may be available through
+// the text api.
+typedef struct {
+    uint8_t sequence:1;                 // if sequence error detection is on and
+                                        // there is a discrepancy in host_timestamp_received
+    uint8_t system:1;                   // Over/undervoltage
+    uint8_t motor:1;                    // Overcurrent, phase problem
+    uint8_t controller:1;               // Tracking error if enabled
+    uint8_t sensor:1;                   // Encoder error
+    uint8_t reserved:3;
+} MotorError;
+
+typedef struct {
+    uint8_t current_mode;               // returns current mode, should be mode_desired
+                                        // unless there is an error
+    MotorError error;                   // \sa MotorError
+    uint16_t reserved;
+} MotorFlags;
 
 typedef struct {
     uint32_t mcu_timestamp;             // timestamp in microcontroller clock cycles
@@ -11,6 +41,8 @@ typedef struct {
     float torque;                       // measured torque in Nm
     int32_t motor_encoder;              // motor position in raw counts
     float reserved[3];
+    MotorFlags flags;                   // \sa MotorFlags
+                                        // 44 bytes
 } MotorStatus;
 
 typedef enum {OPEN, DAMPED, CURRENT, POSITION, TORQUE, IMPEDANCE, VELOCITY, 
@@ -23,6 +55,7 @@ typedef enum {SINE, SQUARE, TRIANGLE, CHIRP} TuningMode;
 typedef struct {
     uint32_t host_timestamp;            // Value from host
     uint8_t mode_desired;               // \sa ModeDesired
+    uint8_t mode_reserved;
     union {
         // main control modes
         struct {                                
@@ -35,36 +68,47 @@ typedef struct {
         
         // debug/tuning modes
         struct {
-            float voltage_desired;
+            float voltage_desired;              // motor voltage V line-line
             float reserved[4];
-        } voltage;
+        } voltage;                              // Close loop voltage mode, may overcurrent easily
         struct {
-            uint32_t mode;
-            float amplitude;
+            uint32_t mode;                      // \sa TuningMode
+            float amplitude;                    // amplitude rad
             float reserved;
-            float bias;
-            float frequency;
+            float bias;                         // bias rad
+            float frequency;                    // frequency Hz
         } position_tuning;
         struct {
-            float amplitude;
-            uint32_t mode;
+            float amplitude;                    // amplitude A
+            uint32_t mode;                      // \sa TuningMode
             float reserved;
-            float bias;
-            float frequency;
+            float bias;                         // bias A
+            float frequency;                    // frequency Hz
         } current_tuning;
         struct {
-            uint32_t mode;
-            float amplitude;
-            float kv;
-            float bias;
-            float frequency;
-        } stepper_tuning;
+            uint32_t mode;                      // \sa TuningMode
+            float amplitude;                    // amplitude in motor radians
+            float kv;                           // motor kv Vs/rad
+            float bias;                         // bias rad
+            float frequency;                    // frequency Hz
+        } stepper_tuning;                       // open loop mode, may skip in position and overcurrent easily
         struct {
-            float voltage;
-            float velocity;
+            float voltage;                      // motor voltage V line-line
+            float velocity;                     // motor velocity rad/s
             float reserved[3];
-        } stepper_velocity;
+        } stepper_velocity;                     // open loop mode, may skip in position and overcurrent easily
     };
+                                            // 28 bytes
 } MotorCommand;
 
-#define MAX_API_DATA_SIZE 1000
+// USB is the current default physical communication protocol. MotorCommand and 
+// MotorStatus structs are sent on one endpoint as defined below. A separate debugging
+// and other communication channel called the text_api is available on on another
+// endpoint. The text_api communicates in 8 bit char non-null terminated strings.
+// The length of the string is provided by the USB protocol and has a max length 
+// defined below 
+
+#define USB_ENDPOINT_MOTOR      2
+#define USB_ENDPOINT_TEXT_API   1
+#define MAX_API_DATA_SIZE 1000      // refers to the TextAPI which is a separate
+                                    // communication channel, both TX and RX
